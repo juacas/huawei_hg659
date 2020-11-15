ade61d33f09fe451e93053b57e9ba58378245484
Shane Meagher <shanemeagher@outlook.com>
2020-11-15 00:12:28 +0000

    Restructure for HACS

---
+++ custom_components/const.py
@@ -0,0 +1,40 @@
+"""Constants for huawei_hg659."""
+# Base component constants
+DOMAIN = "huawei_hg659"
+DOMAIN_DATA = "{}_data".format(DOMAIN)
+VERSION = "0.4.1"
+PLATFORMS = ["sensor"]
+ISSUE_URL = "https://github.com/shanemeagher/huawei_hg659/issues"
+
+STARTUP = """
+-------------------------------------------------------------------
+{name}
+Version: {version}
+This is a custom component
+If you have any issues with this you need to open an issue here:
+{issueurl}
+-------------------------------------------------------------------
+"""
+
+
+# Icons
+ICON = "mdi:router-wireless"
+
+
+ICONS = {
+    'DesktopComputer': 'mdi:desktop-classic',
+    'laptop': 'mdi:laptop',
+    'smartphone': 'mdi:cellphone-wireless',
+    'game': 'mdi:gamepad-variant',
+    'stb': 'mdi:television',
+    'camera': 'mdi:cctv'
+}
+# Configuration
+# CONF_NAME = "name"
+# CONF_SCAN_INTERVAL = "scan_interval"
+
+# Defaults
+DEFAULT_NAME = "Huawei HG659 router"
+
+# Interval in seconds
+INTERVAL = 60
